63ed115c1ccd3b37a588e09d91245f6785c7b60e
jonathan.G <jonathan.gouveia@protonmail.com>
2023-05-24 16:41:36 +0200

    first_commit

---
+++ src/main/java/com/openclassroomsProject/Mediscreennotes/MediscreenNotesApplication.java
@@ -0,0 +1,13 @@
+package com.openclassroomsProject.Mediscreennotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediscreenNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediscreenNotesApplication.class, args);
+	}
+
+}
